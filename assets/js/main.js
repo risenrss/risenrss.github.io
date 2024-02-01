d4e616f4df7182281f0e512f1a37d906547d2bfd
Austin Hughes <austindavidhughes@gmail.com>
2024-01-31 22:21:22 -0500

    many updates
    new youtube videos, more content on main page, replace volunteer page with single google form

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,4 @@ const urlParams = new URLSearchParams(window.location.search);
 const lang = urlParams.get('lang');
 if (lang) {
     document.querySelector('html').setAttribute('lang', lang);
-} else {
-    document.querySelector('html').setAttribute('lang', 'en');
 }
